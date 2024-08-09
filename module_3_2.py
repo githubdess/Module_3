43b2f314aad31846b99ff29bf204c375e993ff4a
githubdess <dess_81@mail.ru>
2024-08-09 14:58:57 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,12 +1,17 @@
 def send_email(message, recipient, sender='university.help@gmail.com'):
-    if '@' not in recipient or not (recipient.endswith('.com' or '.ru' or '.net')):
+    file_extension = ('.com', '.ru', '.net')
+    valid = recipient.endswith(file_extension) == sender.endswith(file_extension)
+    if recipient.count('@') == 1 and sender.count('@') == 1:
+        if valid is not True:
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+        elif recipient == sender:
+            print('Нельзя отправить письмо самому себе!')
+        elif sender == 'university.help@gmail.com':
+            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient} с сообщением {message}.')
+        else:
+            print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender}.')
+    else:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
-    elif recipient == sender:
-        print('Нельзя отправить письмо самому себе!')
-    elif sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient} с сообщением {message}.')
-    elif sender != 'university.help@gmail.com':
-        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
 
 
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', 'urban.teacher@mail.uk')
